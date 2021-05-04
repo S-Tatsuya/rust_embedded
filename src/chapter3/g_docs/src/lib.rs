94c12da5accb002e09edb736335cb0b3ea782db3
S.tatsuya <s.tatsuya0123@gmail.com>
2021-05-04 09:47:46 +0900

    コメントの確認。ドキュメントコメント(///)という機能を初めて知った

---
+++ src/chapter3/g_docs/src/lib.rs
@@ -0,0 +1,15 @@
+/// Doubles the number given.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let result = docs::double(5);
+/// println!("resutl = {}", result);
+/// ```
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
